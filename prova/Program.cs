ce1b0db6acae1883b2fec211f99cb129976114b0
Nikollas Souza <nikollas.souza@etec.sp.gov.br>
2025-04-02 17:40:37 -0300

    prova

---
+++ prova/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            op = "0";
+            Console.WriteLine("/---MENU---/\n\n Escolha uma opção:\n\n 1 - opção 1\n 2 - opção 2\n Q - Sair!");
+            while (op.ToUpper() != "Q")
+            {
+                Console.WriteLine("Deseja continuar?");
+                op = Console.ReadLine();
+
+                if (op.Length < 2) // Verifica se a opção é válida
+                {
+                    switch (op.ToUpper()) // Usar ToUpper() para aceitar tanto "q" quanto "Q"
+                    {
+                        case "1":
+                            Console.WriteLine("Você escolheu a opção 1");
+                            break;
+                        case "2":
+                            Console.WriteLine("Você escolheu a opção 2");
+                            break;
+                        case "Q":
+                            Console.WriteLine("Deseja continuar?");
+                            break;
+                        default:
+                            Console.WriteLine("Você digitou uma opção inválida");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Você digitou uma opção inválida");
+                }
+            }
+        }
+    }
+}
